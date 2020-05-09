e504e0f7ff9478baed232ad3220c7b2c28544ccf
Gregor Riegler <gregor.riegler@ebcont.com>
2020-05-09 17:31:10 +0200

    request a single book

--- test/book-consumer.ts
+++ test/book-consumer.ts
@@ -3,7 +3,47 @@ const bent = require("bent")
 const getJSON = bent("json")
 const expect = require("chai").expect
 
-describe("Book Consumer", () => {
+const requestAListOfBooks = {
+    state: "two books",
+    uponReceiving: "a request for retrieving all books",
+    withRequest: {
+        method: "GET",
+        path: "/books"
+    },
+    willRespondWith: {
+        status: 200,
+        headers: {"Content-Type": "application/json"},
+        body: [
+            {
+                "self": "/books/1",
+                "title": "Hello Book 1"
+            },
+            {
+                "self": "/books/2",
+                "title": "Hello Book 2"
+            }
+        ]
+    },
+};
+
+const requestASingleBook = {
+    state: "two books",
+    uponReceiving: "a request for retrieving the first book",
+    withRequest: {
+        method: "GET",
+        path: "/books/1"
+    },
+    willRespondWith: {
+        status: 200,
+        headers: {"Content-Type": "application/json"},
+        body: {
+            "self": "/books/1",
+            "title": "Hello Book 1"
+        }
+    }
+};
+
+describe("Books Client", () => {
     const producer = new Pact({
         consumer: "Book Consumer",
         provider: "Book Producer",
@@ -11,53 +51,7 @@ describe("Book Consumer", () => {
     })
 
     before(async () => {
-        await producer.setup().then(() => {
-            producer.addInteraction({
-                state: "two books",
-                uponReceiving: "a request for retrieving the book",
-                withRequest: {
-                    method: "GET",
-                    path: "/books"
-                },
-                willRespondWith: {
-                    status: 200,
-                    headers: {"Content-Type": "application/json"},
-                    body: [
-                        {
-                            "self": "/books/1",
-                            "title": "Hello Book 1"
-                        },
-                        {
-                            "self": "/books/2",
-                            "title": "Hello Book 2"
-                        }
-                    ]
-                },
-            })
-        })
-    })
-
-    it("can find all books in a list", async () => {
-        type Book = {
-            self: string
-            title: string
-        }
-
-        function decodeBook(object): Book {
-            return {
-                self: object.self,
-                title: object.title
-            }
-        }
-
-        let response = await getJSON("http://localhost:1234" + "/books")
-
-        let bookList: Array<Book> = response.map(decodeBook)
-
-        expect(bookList).to.deep.equal([
-            {self: "/books/1", title: "Hello Book 1"},
-            {self: "/books/2", title: "Hello Book 2"}
-        ])
+        await producer.setup()
     })
 
     afterEach(() => {
@@ -66,5 +60,60 @@ describe("Book Consumer", () => {
 
     after(() => {
         producer.finalize()
+    })
+
+    describe("requesting a list of books", () => {
+        before(() => {
+            producer.addInteraction(requestAListOfBooks)
+        })
+
+        it("finds the list of books", async () => {
+            type Book = {
+                self: string
+                title: string
+            }
+
+            function decodeBook(object): Book {
+                return {
+                    self: object.self,
+                    title: object.title
+                }
+            }
+
+            let response = await getJSON("http://localhost:1234" + "/books")
+
+            let bookList: Array<Book> = response.map(decodeBook)
+
+            expect(bookList).to.deep.equal([
+                {self: "/books/1", title: "Hello Book 1"},
+                {self: "/books/2", title: "Hello Book 2"}
+            ])
+        })
+    })
+
+    describe("requesting a single book", () => {
+        before(() => {
+            producer.addInteraction(requestASingleBook)
+        })
+
+        it("finds the book", async () => {
+            type Book = {
+                self: string
+                title: string
+            }
+
+            function decodeBook(object): Book {
+                return {
+                    self: object.self,
+                    title: object.title
+                }
+            }
+
+            let response = await getJSON("http://localhost:1234" + "/books/1")
+
+            let book: Book = decodeBook(response)
+
+            expect(book).to.deep.equal({self: "/books/1", title: "Hello Book 1"})
+        })
     })
 });
